e7bfc067e670ea4509bdf7e0e29be57cdee4f866
Ossma1 <toumi11oussama@gmail.com>
2023-11-09 16:48:23 +0100

    ll

--- src/main/java/com/app/entities/Compte.java
+++ src/main/java/com/app/entities/Compte.java
@@ -1,9 +1,34 @@
 package com.app.entities;
 
+import java.io.Serializable;
 
-public class Compte {
-	
-Integer id;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Compte implements Serializable{
+	/**
+	 * 
+	 * 
+	 */
 	
+	@jakarta.persistence.Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer Id;
+	
+    @Column(unique = true,nullable = false)
+	public Integer mail;
+    @Column(nullable = false)
+	public Integer motPass;
+	@Column(unique = true,nullable = false)
+	public Integer userName;
 }
