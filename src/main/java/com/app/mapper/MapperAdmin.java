b4b90de0447fcba8cbf9551fe03ef25b56656c59
abdoufekkak <abdou.fekkak66@gmail.com>
2023-11-09 16:55:57 +0100

    QWER

--- src/main/java/com/app/mapper/MapperAdmin.java
+++ src/main/java/com/app/mapper/MapperAdmin.java
@@ -6,6 +6,9 @@ import com.app.entities.Admin;
 public class MapperAdmin {
    public Admin requestAdminToAdmin(DtoRequestAdmin dtoRequestAdmin) {
 	   Admin admin=new Admin();
-	   
+	   admin.setMdp(dtoRequestAdmin.getMdp());
+	   admin.setNom(dtoRequestAdmin.getNom());
+	   admin.setUsername(dtoRequestAdmin.getUsername());
+	   return admin;
    }
 }
