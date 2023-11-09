24cea67f7db7c26b6f0e318e0e932037e9bba8ff
Salma-Raf <rksalma6@gmail.com>
2023-11-09 18:38:56 +0100

    3

--- src/main/java/com/app/mapper/MapperAdmin.java
+++ src/main/java/com/app/mapper/MapperAdmin.java
@@ -1,8 +1,13 @@
 package com.app.mapper;
 
+import org.springframework.stereotype.Component;
+
+import com.app.dto.DTOResponseAdmin;
 import com.app.dto.DtoRequestAdmin;
 import com.app.entities.Admin;
 
+
+@Component
 public class MapperAdmin {
    public Admin requestAdminToAdmin(DtoRequestAdmin dtoRequestAdmin) {
 	   Admin admin=new Admin();
@@ -11,4 +16,13 @@ public class MapperAdmin {
 	   admin.setUsername(dtoRequestAdmin.getUsername());
 	   return admin;
    }
+   public DTOResponseAdmin adminToResponseDto(Admin admin) {
+	   DTOResponseAdmin	dtoResponseAdmin =new DTOResponseAdmin();
+	   DTOResponseAdmin.setNom(admin.getNom());
+	   DTOResponseAdmin.setId(admin.getId());
+	   DTOResponseAdmin.setUsername(admin.getUsername());
+	   DTOResponseAdmin.setMdp(admin.getMdp());
+
+		return dtoResponseAdmin;
+	} 
 }
